3b6094be85f136e9c02c5e21008ae315b43e6c6a
Cuong Nguyen Ngoc <cuongnguyen71195@gmail.com>
2026-09-24 09:26:17 +0700

    edit new

---
+++ src/ChatWithPdf.Api/DTOs/DocumentChunkDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatWithPdf.Api.DTOs
+{
+    public class DocumentChunkDto
+    {
+    }
+}
+public record DocumentChunkDto(
+    Guid Id,
+    int ChunkIndex,
+    int? PageNumber,
+    string Content,
+    int TokenCount
+);
